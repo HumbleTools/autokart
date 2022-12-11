2714f7c5138ccb4a4eb4a41db801b1b819b83a6a
HumbleTools <louis.madeuf@gmail.com>
2022-12-11 22:55:07 +0100

    refacto(jsonExport): implementing a dedicated component for this feature

--- src/components/admin/AdminMenu.tsx
+++ src/components/admin/AdminMenu.tsx
@@ -1,49 +1,16 @@
-import { useContext, useState } from "react";
-import { toast } from "react-toastify";
-import { LoaderContext } from "../../contexts/LoaderContext";
+import { useContext } from "react";
 import { getSafeUser, UserContext } from "../../contexts/UserContext";
-import { CookbookRecipe, getAllRecipes, mapToCookbookRecipes } from "../../dataServices/RecipeService";
 import { isAdmin } from "../../dataServices/UserService";
-import { usePopin } from "../../hooks/usePopin";
-import { Popin } from "../custom/Popin";
+import { ExportCookbook } from "./ExportCookbook";
 
 export const AdminMenu = () => {
     const userContext = getSafeUser(useContext(UserContext));
-    const { setLoading } = useContext(LoaderContext);
 
-    const popinProps = usePopin();
-    const [cookBookData, setCookBookData] = useState([] as CookbookRecipe[]);
-
-    const handleExportClick = () => {
-        setLoading(true);
-        getAllRecipes()
-        .then(data => {
-            setCookBookData(mapToCookbookRecipes(data));
-            popinProps.toggleDisplay();
-        })
-        .catch((error => {
-            console.error(error);
-            toast.error('Something went wrong...');
-        }))
-        .finally(() => {
-            setLoading(false);
-        });
-    };
-    
     return isAdmin(userContext.roles) ? <>
         <h4 className="mt-1">Menu admin</h4>
         <div className="row button-column">
-            <button className="button-primary" onClick={handleExportClick}>Exporter les recettes</button>
+            <ExportCookbook />
         </div>
-        <Popin {...popinProps}>
-            <h4>Exporter les recettes</h4>
-            Les données sont disponibles au format JSON.<br />
-            <a type="button"
-                href={`data:text/json;charset=utf-16,${encodeURIComponent(JSON.stringify(cookBookData))}`}
-                download="cookbook.json">
-                Télécharger
-            </a>
-        </Popin>
     </>
     : <p>Vous n'êtes pas admin.</p>;
 };
